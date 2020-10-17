15747807577c10d0140ac954d00b9d2afa73c6c1
Lucas Vinicius Bibiano Thome <lucasvbt@trt3.jus.br>
2020-10-17 19:33:44 -0300

    feat: mapeamento e carga dos dados.

---
+++ backend/flask-api/carga_db/carga_tabelas.py
@@ -0,0 +1,67 @@
+import pandas as pd
+from sqlalchemy import create_engine
+engine = create_engine(
+    'postgres://sanjus_app:inovacnj@time16-sanjus.ddns.net:5433/sanjus', echo=False)
+
+df_situacao = pd.read_csv('carga_situacoes.csv', sep=',')
+df_situacao['sg_tribunal'] = 'TRT3'
+df_situacao['sg_grau'] = 'G2'
+df_situacao.to_sql('tb_desc_situacao', con=engine,
+                   if_exists='append', index=False)
+
+df_eventos = pd.read_csv('carga_eventos_fluxo.csv', sep=',')
+df_eventos.info()
+df_eventos_sem_comp = df_eventos[df_eventos['cd_tipo_complemento'].isnull()]
+df_eventos_sem_comp.head()
+
+
+def verificar_registro(sql, cd_pesquisa):
+    with engine.connect() as conn:
+        result = conn.execute(sql, (cd_pesquisa, ))
+        for row in result:
+            return row['id']
+
+
+def carga_complemento(registro, id_movimento):
+    if registro['cd_tipo_complemento'] != 'NaN' and registro['ds_valor_complemento'] != 'NaN':
+        sql = """Insert into tb_desc_complemento values (nextval('tb_desc_complemento_id_complemento_seq'),%s, %s, %s)                
+        """
+        with engine.connect() as conn:
+            conn.execute(sql, (registro['cd_tipo_complemento'],
+                               id_movimento, registro['ds_valor_complemento']))
+
+
+def carga_movimento(registro, id_evento):
+    id_movimento = verificar_registro(
+        'select id_movimento as id from tb_desc_movimento where cd_tpu_movimento = %s', str(registro['cd_movimento']))
+    if registro['cd_movimento'] != 'NaN' and id_movimento is None:
+        sql = """Insert into tb_desc_movimento values (nextval('tb_desc_movimento_id_movimento_seq'), %s, %s)              
+        """
+        with engine.connect() as conn:
+            conn.execute(sql, (registro['cd_movimento'], id_evento))
+            id_movimento = verificar_registro(
+                'select id_movimento as id from tb_desc_movimento where cd_tpu_movimento = %s', str(registro['cd_movimento']))
+    return carga_complemento(registro, id_movimento)
+
+
+def carga_evento(registro):
+    id_evento = verificar_registro(
+        'select id_evento as id from tb_desc_evento where cd_evento = %s', registro['cd_evento'])
+    if id_evento is None:
+        sql = """Insert into tb_desc_evento values (nextval('tb_desc_evento_id_evento_seq'), %s, %s, %s)              
+        """
+        with engine.connect() as conn:
+            conn.execute(
+                sql, (registro['ds_evento'], registro['ind_fluxo_ri'], registro['cd_evento']))
+            id_evento = verificar_registro(
+                'select id_evento as id from tb_desc_evento where cd_evento = %s', registro['cd_evento'])
+    return carga_movimento(registro, id_evento)
+
+
+df_eventos.apply(carga_evento, axis=1)
+
+df_grp_situacoes = pd.read_csv('carga_grp_situacoes.csv', sep=',')
+df_grp_situacoes['sg_tribunal'] = 'TRT3'
+df_grp_situacoes['sg_grau'] = 'G2'
+df_grp_situacoes.to_sql('tb_desc_grp_situacao', con=engine,
+                        if_exists='append', index=False)
--- backend/flask-api/src/entidades/modelo_fluxo.py
+++ backend/flask-api/src/entidades/modelo_fluxo.py
@@ -2,14 +2,26 @@ from sqlalchemy.orm import relationship
 from ..persistencia.database import db
 
 
+class Evento(db.Model):
+    __tablename__ = 'tb_desc_evento'
+
+    id_evento = db.Column(db.Integer, primary_key=True)
+    ds_evento = db.Column(db.String(255), nullable=False)
+    cd_evento = db.Column(db.String(30), nullable=False)
+    ind_fluxo_ri = db.Column(db.String(1), nullable=False)
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+
 class Movimento(db.Model):
     __tablename__ = 'tb_desc_movimento'
 
     id_movimento = db.Column(db.Integer, primary_key=True)
-    ds_movimento = db.Column(db.String(255), nullable=False)
     cd_tpu_movimento = db.Column(db.String(50), nullable=False)
-    sg_movimento = db.Column(db.String(30), nullable=False)
-    cd_tpu_complemento = db.Column(db.String(255), nullable=False)
+    id_evento = db.Column(db.Integer, db.ForeignKey(
+        'tb_desc_evento.id_evento'), nullable=False)
 
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
@@ -24,6 +36,8 @@ class Situacao(db.Model):
     cd_situacao = db.Column(db.String(50), nullable=False)
     ind_principal = db.Column(db.String(1), nullable=False)
     ind_ri = db.Column(db.String(1), nullable=False)
+    sg_tribunal = db.Column(db.String(30), nullable=False)
+    sg_grau = db.Column(db.String(30), nullable=False)
 
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
@@ -36,6 +50,8 @@ class GrupoSituacao(db.Model):
     id_grupo = db.Column(db.Integer, primary_key=True)
     ds_grupo = db.Column(db.String(255), nullable=False)
     cd_grupo = db.Column(db.String(50), nullable=False)
+    sg_tribunal = db.Column(db.String(30), nullable=False)
+    sg_grau = db.Column(db.String(30), nullable=False)
 
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
@@ -45,17 +61,79 @@ class GrupoSituacao(db.Model):
 class FluxoMovimentos(db.Model):
     __tablename__ = 'tb_fluxo'
 
-    id_fluxo_movimento = db.Column(db.String(255), nullable=False, primary_key=True)
+    id_fluxo_movimento = db.Column(
+        db.Integer, nullable=False, primary_key=True)
     id_situacao_origem = db.Column(db.Integer, db.ForeignKey(
         'tb_desc_situacao.id_situacao'), nullable=False)
-    id_movimento = db.Column(db.Integer, db.ForeignKey(
-        'tb_desc_movimento.id_movimento'), nullable=False)
+    id_evento = db.Column(db.Integer, db.ForeignKey(
+        'tb_desc_evento.id_evento'), nullable=False)
     id_situacao_destino = db.Column(db.Integer, db.ForeignKey(
         'tb_desc_situacao.id_situacao'), nullable=False)
     ind_consistente = db.Column(db.String(1), nullable=False)
     ind_efetiva = db.Column(db.String(1), nullable=False)
+    ind_fluxo_ri = db.Column(db.String(1), nullable=False)
     id_grupo = db.Column(db.Integer, db.ForeignKey(
         'tb_desc_grp_situacao.id_grupo'), nullable=False)
+    sg_tribunal = db.Column(db.String(30), nullable=False)
+    sg_grau = db.Column(db.String(30), nullable=False)
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+
+class HistoricoEvento(db.Model):
+    __tablename__ = 'tb_hist_evento'
+
+    id_hist_evento = db.Column(db.Integer, nullable=False, primary_key=True)
+    cd_processo = db.Column(db.String(50), nullable=False)
+    cd_classe_judicial = db.Column(db.String(20), nullable=False)
+    id_evento = db.Column(db.Integer, db.ForeignKey(
+        'tb_desc_evento.id_evento'), nullable=False)
+    dt_ocorrencia = db.Column(db.Time, nullable=False)
+    ind_ri = db.Column(db.String(1), nullable=False)
+    nu_autuacao = db.Column(db.Integer, nullable=False)
+    sg_tribunal = db.Column(db.String(30), nullable=False)
+    sg_grau = db.Column(db.String(30), nullable=False)
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+
+class HistoricoSituacao(db.Model):
+    __tablename__ = 'tb_hist_situacao'
+
+    id_hist_situacao = db.Column(db.Integer, nullable=False, primary_key=True)
+    cd_processo = db.Column(db.String(50), nullable=False)
+    cd_classe_judicial = db.Column(db.String(20), nullable=False)
+    nu_autuacao = db.Column(db.Integer, nullable=False)
+    id_situacao_origem = db.Column(db.Integer, db.ForeignKey(
+        'tb_desc_situacao.id_situacao'), nullable=False)
+    id_situacao_destino = db.Column(db.Integer, db.ForeignKey(
+        'tb_desc_situacao.id_situacao'), nullable=False)
+    id_movimento = db.Column(db.Integer, db.ForeignKey(
+        'tb_desc_movimento.id_movimento'), nullable=False)
+    dt_ocorrencia = db.Column(db.Time, nullable=False)
+    ind_valida = db.Column(db.String(1), nullable=False)
+    ind_consistente = db.Column(db.String(1), nullable=False)
+    ind_efetiva = db.Column(db.String(1), nullable=False)
+    sg_tribunal = db.Column(db.String(30), nullable=False)
+    sg_grau = db.Column(db.String(30), nullable=False)
+
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+
+class Complemento(db.Model):
+    __tablename__ = 'tb_desc_complemento'
+
+    id_complemento = db.Column(db.Integer, primary_key=True)
+    cd_tpu_complemento = db.Column(db.String(50), nullable=False)
+    vl_complemento = db.Column(db.String(400), nullable=False)
+    id_movimento = db.Column(db.Integer, db.ForeignKey(
+        'tb_desc_movimento.id_movimento'), nullable=False)
 
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
